1e4ba80dc1773f2b904fb17f098a8871a7a113fd
Gary Verhaegen <gary.verhaegen@gmail.com>
2016-05-24 23:20:48 +0100

    Remove obsolete comments

--- src/clj/atlas/core.clj
+++ src/clj/atlas/core.clj
@@ -27,46 +27,6 @@
 
   (stop-dev-server)
 
-  ;; $ curl -v -X POST -d '{"username": "mtgred", "password": "password"}' \
-  ;;           -H 'Content-Type: application/json; charset=utf-8' \
-  ;;           localhost:2442/login
-  ;; *   Trying ::1...
-  ;; * Connected to localhost (::1) port 2442 (#0)
-  ;; > POST /login HTTP/1.1
-  ;; > Host: localhost:2442
-  ;; > User-Agent: curl/7.43.0
-  ;; > Accept: */*
-  ;; > Content-Type: application/json; charset=utf-8
-  ;; > Content-Length: 46
-  ;; >
-  ;; * upload completely sent off: 46 out of 46 bytes
-  ;; < HTTP/1.1 200 OK
-  ;; < Set-Cookie: ring-session=1dd79a35-8bb8-42da-9213-ff41dcf68623;Path=/;HttpOnly
-  ;; < Content-Type: application/json; charset=utf-8
-  ;; < Content-Length: 11
-  ;; < Server: http-kit
-  ;; < Date: Sun, 22 May 2016 16:49:08 GMT
-  ;; <
-  ;; * Connection #0 to host localhost left intact
-  ;; ["success"]
-  ;; $ curl -v --cookie 'ring-session=1dd79a35-8bb8-42da-9213-ff41dcf68623;Path=/;HttpOnly' localhost:2442/session
-  ;; *   Trying ::1...
-  ;; * Connected to localhost (::1) port 2442 (#0)
-  ;; > GET /session HTTP/1.1
-  ;; > Host: localhost:2442
-  ;; > User-Agent: curl/7.43.0
-  ;; > Accept: */*
-  ;; > Cookie: ring-session=1dd79a35-8bb8-42da-9213-ff41dcf68623;Path=/;HttpOnly
-  ;; >
-  ;; < HTTP/1.1 200 OK
-  ;; < Content-Type: application/json; charset=utf-8
-  ;; < Content-Length: 21
-  ;; < Server: http-kit
-  ;; < Date: Sun, 22 May 2016 16:49:36 GMT
-  ;; <
-  ;; * Connection #0 to host localhost left intact
-  ;; {"identity":"mtgred"}
-
   (require '[clojurewerkz.scrypt.core :as sc])
 
   (sc/encrypt "password" 16384 8 1)
