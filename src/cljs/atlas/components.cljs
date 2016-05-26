8b6f1dba16fe9b0260ba07688efd8d333e0a527e
Minh Tran <mtgred@gmail.com>
2016-05-26 03:43:51 +0100

    Navbar and user components

--- src/cljs/atlas/components.cljs
+++ src/cljs/atlas/components.cljs
@@ -1,5 +1,6 @@
 (ns atlas.components
   (:require [reagent.core :as r]
+            [re-frame.core :refer [subscribe]]
             [atlas.routes :as routes]))
 
 (defn link [{:keys [href] :as props} children]
@@ -14,3 +15,19 @@
                  :value @state
                  :on-change #(reset! state (-> % .-target .-value)))])
 
+(defn user-view
+  ([user] (user-view user 24))
+  ([{:keys [username email-hash] :as user} size]
+   [:div
+    [:img {:src (str "http://www.gravatar.com/avatar/" email-hash "?d=retro&s=" size)}]
+    username]))
+
+(defn navbar []
+  (let [current-user (subscribe [:current-user])]
+    (fn []
+     [:div.navbar
+      (if @current-user
+        [user-view @current-user]
+        [:div
+         [link {:href "/register"} "Register"]
+         [link {:href "/login"} "Login"]])])))
